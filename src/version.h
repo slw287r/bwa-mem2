22a71a9ae7326134f67bda7da9ee9c6a56a79a9d
slw287r <slw287r@163.com>
2024-09-20 22:30:13 +0800

    bumpped version

--- src/version.h
+++ src/version.h
@@ -1,8 +1,10 @@
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "2.2.1d"
+#define PACKAGE_VERSION "2.2.1e"
 #endif
 
 #undef BRANCH_COMMIT
 #define BRANCH_COMMIT "mmap 80f894d"
 #undef BRANCH_COMMIT
 #define BRANCH_COMMIT "mmap-izlib 2007242"
+#undef BRANCH_COMMIT
+#define BRANCH_COMMIT "mmap-izlib c713271"
