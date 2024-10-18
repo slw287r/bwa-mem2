65c9f9c22a129bb0d0dfe4fc3e9cde629fccac49
liujc <liujc@geneplus.cn>
2024-10-18 10:23:54 +0800

    bumps version

--- src/version.h
+++ src/version.h
@@ -2,11 +2,4 @@
 #define PACKAGE_VERSION "2.2.1e"
 #endif
 
-#undef BRANCH_COMMIT
-#define BRANCH_COMMIT "mmap 80f894d"
-#undef BRANCH_COMMIT
-#define BRANCH_COMMIT "mmap-izlib 2007242"
-#undef BRANCH_COMMIT
-#define BRANCH_COMMIT "mmap-izlib c713271"
-#undef BRANCH_COMMIT
-#define BRANCH_COMMIT "mmap-izlib a63852b"
+#define BRANCH_COMMIT "mmap-izlib 152a90a"
